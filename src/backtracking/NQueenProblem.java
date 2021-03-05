86a4e0e7984943c0d7c42a9edbd8f003b6b9acec
Hilal Kaldane <hilalkaldane.kaldane937@gmail.com>
2021-03-05 20:37:07 +0530

    adds package name

--- src/backtracking/NQueenProblem.java
+++ src/backtracking/NQueenProblem.java
@@ -1,3 +1,4 @@
+package backtracking;
 public class NQueenProblem
 {
     public static int queens = 0;
--- src/backtracking/RatInMaze.java
+++ src/backtracking/RatInMaze.java
@@ -1,3 +1,4 @@
+package backtracking;
 public class RatInMaze
 {
     public static void main(String[] args)
